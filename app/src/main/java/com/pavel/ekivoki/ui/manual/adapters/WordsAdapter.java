07ff2134699db9b1e7a54bb0745bf2d38832a07e
lubuntum <lubuntum@gmail.com>
2022-09-09 22:50:56 +0700

    Добавил фрагмент для загрузки категорий + Адаптер для слов и разметку для списка слов

---
+++ app/src/main/java/com/pavel/ekivoki/ui/manual/adapters/WordsAdapter.java
@@ -0,0 +1,43 @@
+package com.pavel.ekivoki.ui.manual.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pavel.ekivoki.R;
+import com.pavel.ekivoki.data.entity.Word;
+
+import java.util.List;
+
+public class WordsAdapter extends ArrayAdapter<Word> {
+    private LayoutInflater layoutInflater;
+    private int resource;
+    private List<Word> words;
+
+
+    public WordsAdapter(@NonNull Context context, int resource, @NonNull List<Word> words) {
+        super(context, resource, words);
+        this.layoutInflater = LayoutInflater.from(context);
+        this.words = words;
+        this.resource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null){
+            convertView = layoutInflater.inflate(resource,parent,false);
+        }
+        String word = words.get(position).getWord();
+        TextView wordTextView = convertView.findViewById(R.id.wordTextView);
+        wordTextView.setText(word);
+        return convertView;
+    }
+}
